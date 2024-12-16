3e701618735c501b2c22fd569b3bf19584296446
hzihhuang <hzihhuang@163.com>
2024-12-16 03:30:42 +0000

    perf: 归零

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HomeModule } from './home/home.module';
 
 @Module({
-  imports: [HomeModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
